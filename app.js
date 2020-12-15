fe8de6bf59e412ec543b17983223c73eca17dc20
Yathavan Push <pushyathavan@gmail.com>
2020-12-15 12:25:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const key = process.env.SECRET;
+userSchema.plugin(encrypt, { secret: key, encrypytedFields: ["password"] });
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+//initialize server
+const port = 3000;
+app.listen(port, function () {
+  console.log("Server running on Port:" + port + "...");
+});
