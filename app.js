dd3d642672fbad20e829ec6bb5b36f091e6235d4
Yathavan Push <pushyathavan@gmail.com>
2021-01-11 01:32:58 -0500

    final changes to app.js

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/secrets",
+      callbackURL:
+        "https://dry-retreat-29280.herokuapp.com/auth/google/secrets",
     },
     function (accessToken, refreshToken, profile, cb) {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
